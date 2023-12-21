c3cc5279f16b87f32942bb0840259566dc9d7874
faruk.yld <123uzunince123@gmail.com>
2023-12-21 19:17:53 +0300

    init spring boot project

---
+++ printapp/src/main/java/farukyld/printapp/PrintappApplication.java
@@ -0,0 +1,13 @@
+package farukyld.printapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PrintappApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PrintappApplication.class, args);
+	}
+
+}
---
+++ printapp/src/test/java/farukyld/printapp/PrintappApplicationTests.java
@@ -0,0 +1,13 @@
+package farukyld.printapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PrintappApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
